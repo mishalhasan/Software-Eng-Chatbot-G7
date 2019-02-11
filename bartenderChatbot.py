5253eaffeb23ebd85bd8d971b8fd98ec68b7982a
Raphael Chevallier <raphaelchevallier@hotmail.com>
2019-02-10 21:18:37 -0800

    added processing on input to see if we can manipulate the user input

--- bartenderChatbot.py
+++ bartenderChatbot.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask import request
+import json
 
 def createMessage(input): #change this method and anything to create the message wanted to facebook. input paramter is message coming in
-    return 'JSON posted'  #this will return the wanted message back out to messenger
+    data = input['text'] #grab the text of the input json. Now the input is just a string 'data' to work with
+
+    print("what the input is: ") #testing the input and doing some processes to see if we can respond correctly. We can!
+    print(data)
+    print(type(data))
+    data = data + " I am echoing what you say!"
+    data = data + " I am doing some processes on the input rn! This works so well"
+    return data  #this will return the wanted message back out to messenger
 
 
 
