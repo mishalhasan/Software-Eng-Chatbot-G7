1fa1624c17bf3f1ad407c5347f678f79ef9a87c7
Aaron Mahnic <aaron@navigatormm.com>
2019-02-13 14:55:37 -0800

    Check for greetings

--- bartenderChatbot.py
+++ bartenderChatbot.py
@@ -1,23 +1,37 @@
 from flask import Flask
 from flask import request
+from textblob import TextBlob
 import json
 
 def createMessage(input): #change this method and anything to create the message wanted to facebook. input paramter is message coming in
-    data = input['text'] #grab the text of the input json. Now the input is just a string 'data' to work with
+    input_msg = TextBlob(input['text'])
+    data = respond(input_msg)
+    #print("what the input is: ") #testing the input and doing some processes to see if we can respond correctly. We can!
+    #print(data)
+    #print(type(data))
+    #data = data + " I am echoing what you say!"
+    #data = data + " I am doing some processes on the input rn! This works so well"
 
-    print("what the input is: ") #testing the input and doing some processes to see if we can respond correctly. We can!
-    print(data)
-    print(type(data))
-    data = data + " I am echoing what you say!"
-    data = data + " I am doing some processes on the input rn! This works so well"
     return data  #this will return the wanted message back out to messenger
 
 
 
+GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up",)
+GREETING_RESPONSES = ["What can I get for ya?", "You again?"]
 
 
+def respond(input_msg):
+    if check_for_greeting(input):
+        return random.choice(GREETING_RESPONSES)
+    else:
+        return input_msg
 
 
+def check_for_greeting(sentence):
+    for word in sentence.words:
+        if word.lower() in GREETING_KEYWORDS:
+            return True
+    return False
 
 
 #all code above this created API
