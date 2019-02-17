3c3fefac3d821b2d8bd8cfc18e6eb8a3e36d2987
Aaron Mahnic <aaron@navigatormm.com>
2019-02-17 08:46:19 -0800

    Passed user session Id's from node to flask, created a dictionary to store sessions to be able to see how far along the conversation is

--- bartenderChatbot.py
+++ bartenderChatbot.py
@@ -6,33 +6,41 @@ import json
 
 def createMessage(input): #change this method and anything to create the message wanted to facebook. input paramter is message coming in
     input_msg = TextBlob(input['text'])
-    data = respond(input_msg)
+    senderId = input['sid']
+    data = respond(input_msg, senderId)
+    print(chat_log)
     #print("what the input is: ") #testing the input and doing some processes to see if we can respond correctly. We can!
     #print(data)
     #print(type(data))
     #data = data + " I am echoing what you say!"
     #data = data + " I am doing some processes on the input rn! This works so well"
-
     return str(data)  #this will return the wanted message back out to messenger
 
 
+def how_many_messages(senderId):
+    if senderId not in chat_log:
+        log = {'times_contacted': 1, 'context': 'null'}
+        chat_log[senderId] = log
+        return 1
+    else:
+        times_con = chat_log[senderId]['times_contacted'] + 1
+        chat_log[senderId]['times_contacted'] = times_con
+        return times_con
 
-GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up",)
-GREETING_RESPONSES = ["What can I get for ya?", "You again?"]
+
+GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up", "hi there")
+GREETING_RESPONSES = ["What can I get you?", "Hello stranger, what can I serve up for you?", "Need a drink?", "Hi, hope you're thirsty. What can I get you?"]
+chat_log = {}
 
 
-def respond(input_msg):
-    if check_for_greeting(input_msg):
+def respond(input_msg, senderId):
+    times_con = how_many_messages(senderId)
+    if check_for_greeting(input_msg) and times_con == 1:
         return random.choice(GREETING_RESPONSES)
+    if times_con > 3:
+        return "You are too drunk I am unable to serve you any more drinks"
     else:
         return input_msg
-
-
-def check_for_greeting(sentence):
-    for word in sentence:
-        if word.lower() in GREETING_KEYWORDS:
-            return True
-    return False
 
 
 #all code above this created API
