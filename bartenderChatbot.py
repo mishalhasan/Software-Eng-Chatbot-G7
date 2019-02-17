4d739702e41a1d689143468009868d3f6723c7a4
Aaron Mahnic <aaron@navigatormm.com>
2019-02-17 09:53:33 -0800

    added some simple text processing

--- bartenderChatbot.py
+++ bartenderChatbot.py
@@ -3,6 +3,15 @@ from flask import request
 from textblob import TextBlob
 import random
 import json
+import os
+
+os.environ['NLTK_DATA'] = '~/nltk_data'
+GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up", "hi there")
+GREETING_RESPONSES = ["What can I get you?", "Hello stranger, what can I serve up for you?", "Need a drink?", "Hi, hope you're thirsty. What can I get you?"]
+HEDGE_RESPONSES = ["I have no idea what you're asking", "I'm not sure", "Can you re-phrase that?", "Pardon?", "Sorry I can't do that", "I'm confused"]
+DRINKS = ("vodka", "beer", "whiskey", "wine")
+chat_log = {}
+
 
 def createMessage(input): #change this method and anything to create the message wanted to facebook. input paramter is message coming in
     input_msg = TextBlob(input['text'])
@@ -28,11 +37,6 @@ def how_many_messages(senderId):
         return times_con
 
 
-GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up", "hi there")
-GREETING_RESPONSES = ["What can I get you?", "Hello stranger, what can I serve up for you?", "Need a drink?", "Hi, hope you're thirsty. What can I get you?"]
-chat_log = {}
-
-
 def respond(input_msg, senderId):
     if input_msg.lower() == 'clear':
         chat_log[senderId]['times_contacted'] = 0
@@ -40,10 +44,80 @@ def respond(input_msg, senderId):
     times_con = how_many_messages(senderId)
     if check_for_greeting(input_msg) and times_con == 1:
         return random.choice(GREETING_RESPONSES)
-    if times_con > 3:
+    pronoun, noun, adjective, verb = get_speech_parts(input_msg)
+    num_drinks = chat_log[senderId]['drinks_served']
+    if num_drinks > 3:
         return "You are too drunk I am unable to serve you any more drinks. You can type 'clear' to tell me that you're sober again"
-    else:
-        return input_msg
+    if noun:
+        if noun in DRINKS:
+            #increment drink counter
+            chat_log[senderId]['drinks_served'] = num_drinks + 1
+            if num_drinks > 1:
+                return "One {0} coming right up!".format(noun)
+            if num_drinks > 1:
+                return "Heres your {0}! Wow you've already had {1} drinks!".format(noun, num_drinks)
+    return random.choice(HEDGE_RESPONSES)
+
+
+def check_for_greeting(sentence):
+    for word in sentence.words:
+        if word.lower() in GREETING_KEYWORDS:
+            return True
+    return False
+
+
+def get_speech_parts(input_msg):
+    pronoun = None
+    noun = None
+    adjective = None
+    verb = None
+    for s in input_msg.sentences:
+        pronoun = find_pronoun(s)
+        noun = find_noun(s)
+        adjective = find_adjective(s)
+        verb = find_verb(s)
+    return pronoun, noun, adjective, verb
+
+
+def find_pronoun(sent):
+    pronoun = None
+    for word, part_of_speech in sent.pos_tags:
+        if part_of_speech == 'PRP' and word.lower() == 'you':
+            pronoun = 'I'
+        elif part_of_speech == 'PRP' and word == 'I':
+            pronoun = 'You'
+    return pronoun
+
+
+def find_verb(sent):
+    verb = None
+    pos = None
+    for word, part_of_speech in sent.pos_tags:
+        if part_of_speech.startswith('VB'):  # This is a verb
+            verb = word
+            pos = part_of_speech
+            break
+    return verb, pos
+
+
+def find_noun(sent):
+    noun = None
+    if not noun:
+        for w, p in sent.pos_tags:
+            if p == 'NN' and w != 'i':  # This is a noun
+                #textblob detecting 'i' as a noun
+                noun = w
+                break
+    return noun
+
+
+def find_adjective(sent):
+    adj = None
+    for w, p in sent.pos_tags:
+        if p == 'JJ':  # This is an adjective
+            adj = w
+            break
+    return adj
 
 
 #all code above this created API
@@ -59,4 +133,3 @@ def postJsonHandler():
     return message
 
 app.run(host = '0.0.0.0', port = 8090)
-
